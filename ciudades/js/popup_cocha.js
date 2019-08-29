28e8737676cf09bc96b4e2acf2cb8dfd393cf2c4
magne754 <magne754@gmail.com>
2019-08-29 10:26:52 -0400

    actualazar

---
+++ ciudades/js/popup_cocha.js
@@ -0,0 +1,65 @@
+var OpenPopUp = document.getElementById("open-popup"),
+    overlay = document.getElementById("overlay"),
+    popup = document.getElementById("popup"),
+    closePopup = document.getElementById("close-popup");
+var requestURL = 'https://requebernardo.github.io/tecnologia-web/asignaciones/asignacion_9/ciudades/JOSN/tiwanaku.json';
+var request = new XMLHttpRequest();
+var information = document.querySelector('section');;
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+
+function showplace(jsonObj,a) {
+    var lugares = jsonObj['places'];
+    for (var i = 0; i < lugares.length; i++) {
+        
+        if(lugares[i].place == a){
+        var myArticle = document.createElement('article');
+        var myH1 = document.createElement('h1');
+        var myH2 = document.createElement('h2');
+        var myPara = document.createElement('p');
+
+        myH1.textContent = lugares[i].city;
+        myH2.textContent =  lugares[i].place;
+        myPara.textContent = lugares[i].description;
+        }
+
+        myArticle.appendChild(myH1);
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara);
+
+        information.appendChild(myArticle);
+    }
+}
+open-popupu.addEventListener('click', function(){
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Arani');
+});
+open-popupd.addEventListener('click', function(){
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Cliza');
+});
+open-popupt.addEventListener('click', function(){
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Punata');
+});
+open-popupc.addEventListener('click', function(){
+    var lugar = request.response;
+    request.onload = lugar;
+    overlay.classList.add('active');
+    showplace(lugar, 'Tarata');
+});
+
+
+closePopup.addEventListener('click', function(){
+    document.querySelector('section').innerHTML='';
+    overlay.classList.remove('active')
+});
+
